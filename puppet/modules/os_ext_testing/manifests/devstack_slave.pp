a1968ecc3bb9e6e77cc1e3ac9e1f24b9f51e03a0
Ramy Asselin <ramy.asselin@hp.com>
2014-07-18 13:20:33 -0700

    Switch to upstream jenkins slave setup

--- puppet/modules/os_ext_testing/manifests/devstack_slave.pp
+++ puppet/modules/os_ext_testing/manifests/devstack_slave.pp
@@ -10,6 +10,7 @@ class os_ext_testing::devstack_slave (
 ) {
   include os_ext_testing::base
   include openstack_project::tmpcleanup
+  include openstack_project
   class { 'jenkins::slave':
     bare         => $bare,
     ssh_key      => $ssh_key,
@@ -17,4 +18,27 @@ class os_ext_testing::devstack_slave (
     include_pypy => $include_pypy,
   }
   include devstack_host
+
+  file { '/home/jenkins/cache/':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+  file { '/home/jenkins/cache/files/':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => file['/home/jenkins/cache/'],
+  }
+
+  file { '/srv/static/logs/robots.txt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require => File['/srv/static/logs'],
+  }
+
 }
